a9a1bfdb8978593e66376ec17349adb78382c058
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-16 22:13:28 +0000

    Data types of variables checked

--- py-basics.py
+++ py-basics.py
@@ -18,6 +18,15 @@ than one line...
 #variables
 number = 9
 
+#casting to specify a data type
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print('x: ',x, ', y: ',y,',z: ',z)
+print('typeof x: ',type(x), ', typeof y: ',type(y),', typeof z: ',type(z))
+
 #if else condition
 if(number%2==0):
     print(number, "is a even number")
