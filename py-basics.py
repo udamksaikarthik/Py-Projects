468ed10c521fbb400876e98f9daed875034e1d12
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-16 22:07:59 +0000

    Added comments

--- py-basics.py
+++ py-basics.py
@@ -1,5 +1,7 @@
+#imports
 import sys
 
+#print statements...
 print(sys.version)
 
 print('Hello world')
@@ -7,8 +9,10 @@ print("I am learning python")
 print('It is awesome', end=" ")
 print('and fun')
 
+#variables
 number = 9
 
+#if else condition
 if(number%2==0):
     print(number, "is a even number")
 else:
