6e49934d23102fe9c4dddc530803e60eb6c9df80
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-17 10:53:51 +0000

    Refactored classes including methods inside classes

--- classes.py
+++ classes.py
@@ -4,23 +4,33 @@ Class is a blue print
 
 class Car:
     # similar to constructor concept
-    def __init__(self, color: str, horsepower: int) -> None:
+    # self keyword references to instance of the class
+    def __init__(self, brand: str, color: str, horsepower: int) -> None:
+        self.brand = brand
         self.color = color
         self.horsepower = horsepower
 
-alto: Car = Car('Grey', 2500)
-volkswagen: Car = Car('Red', 4800)
-ferrari: Car = Car('White', 9800)
+
+    # method
+    def drive(self) -> None:
+        print(f'{self.brand} is driving')
+
+alto: Car = Car('Maruthi Suzuki Alto','Grey', 2500)
+volkswagen: Car = Car('Volkswagen','Red', 4800)
+ferrari: Car = Car('Ferrari','White', 9800)
 print('Alto')
 print('-------------------------------------------')
+alto.drive()
 print('Color: ', alto.color,'Horsepower: ', alto.horsepower)
 print('-------------------------------------------')
 print('Volkswagen')
 print('-------------------------------------------')
+volkswagen.drive()
 print('Color: ',volkswagen.color,'Horsepower: ', volkswagen.horsepower)
 print('-------------------------------------------')
 print('Ferrari')
 print('-------------------------------------------')
+ferrari.drive()
 print('Color: ',ferrari.color,'Horsepower: ', ferrari.horsepower)
 print('-------------------------------------------')
 
