f942abba501015370abd02388dfb18a916667bfb
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-17 10:12:39 +0000

    Basic data types

---
+++ basic-datatypes.py
@@ -0,0 +1,15 @@
+#datatypes
+
+number: int = 10
+decimal: float = 1.5
+name: str = "Karthik!"
+active: bool = False
+
+# list is a collection of different data types
+names: list = ['Karthik', 'Gayathri', 'Rudhvedh',21, 20, 18] 
+# tuples are similar to lists, but tuples are immutable and once declared can't be changed
+coordinates: tuple = (1.5, 1.5, 2.5)
+# Set is similat to lists as well, but can't have duplicates. (unique)
+unique: set = {1, 2, 2, 4, 9}
+# key value pair system.
+data: dict = {'name': 'Karthik', 'age': 26}
