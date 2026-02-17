6d161a75cf496b2b8e5460b2266bdf30d3ae8aba
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-17 13:33:21 +0000

    Added macth, nested if

--- conditional-statements.py
+++ conditional-statements.py
@@ -1,7 +1,33 @@
 age = input('Enter your age:')
 
 age = int(age)
+
 if age>=18:
     print('you are Eligible to vote.')
 else:
     print('You are not eligible to vote yet, because ur age is less than 18')
+
+
+# Nested If
+
+num = int(age)
+
+if num > 5:
+    if num > 9:
+        print(f"{num} is greater than 5 and 9")
+    else:
+        print(f"{num} is less than 9 and greater than 5")
+else:
+    print(f'{num} is less than 5')
+
+# match (similar to switch from other languages)
+
+match num:
+    case 1:
+        print('one')
+    case 2:
+        print('two')
+    case 3:
+        print('three')
+    case _:
+        print('someother number')
