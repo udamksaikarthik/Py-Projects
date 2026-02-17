05428d4252292d5b51a7e279821aafa95e52c6e4
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-17 11:52:50 +0000

    ifelse conditional statement

---
+++ conditional-statements.py
@@ -0,0 +1,7 @@
+age = input('Enter your age:')
+
+age = int(age)
+if age>=18:
+    print('you are Eligible to vote.')
+else:
+    print('You are not eligible to vote yet, because ur age is less than 18')
