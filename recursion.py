7c603215e0ece023a19aeba4fa58c2d86674b005
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-18 21:56:42 +0000

    refactor: recursion

--- recursion.py
+++ recursion.py
@@ -1,7 +1,16 @@
+import sys
+from time import sleep
+sys.setrecursionlimit(1000)
+print(sys.getrecursionlimit())
+
+count = 1
 
 # function calling itself is called recursion
 def greet():
-    print('Hello!')
+    global count
+    print('Hello!', count)
+    count = count + 1
+    sleep(0.01)
     greet()
 
 greet()
