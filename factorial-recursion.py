3d9a04c059589f1dc5b4a8bfdae53a21dfadfb53
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-19 19:07:10 +0000

    Refactored

--- factorial-recursion.py
+++ factorial-recursion.py
@@ -1,15 +1,23 @@
 
-res = 1
+# res = 1
 
-def fact(a: int) -> None:
-    if(a==0):
-        return
-    global res
-    res = res * a
-    a = a - 1
-    fact(a)
+# def fact(a: int) -> None:
+#     if(a==0):
+#         return
+#     global res
+#     res = res * a
+#     a = a - 1
+#     fact(a)
 
-fact(5)
+# fact(5)
 
     
-print(res)
+# print(res)
+
+
+def fact(a: int) -> int:
+    if a == 1:
+        return 1
+    return a * fact( a - 1)
+
+print(fact(5))
