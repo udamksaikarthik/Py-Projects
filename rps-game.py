f49ed2fbf3beb8a0717ddabd0d3a28bc80fb037d
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-20 11:28:32 +0000

    rps game initial commit

---
+++ rps-game.py
@@ -0,0 +1,8 @@
+
+print('Rock Paper Scissor Game')
+print('-------------------------------------')
+print('R-Rock')
+print('P-Paper')
+print('S-Scissor')
+
+user_input = input("Enter your choice(R/P/S):")
